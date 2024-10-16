ce60b515a03c967d1314d71108d683a9fd86a17d
mchlkrpch <143273709+mchlkrpch@users.noreply.github.com>
2024-10-16 20:31:45 +0300

    Update value_types.hpp

--- type_lists/value_types.hpp
+++ type_lists/value_types.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
+#include <type_tuples.hpp>
 
 namespace value_types
-{
+{ template<auto V>
+	struct ValueTag{ static constexpr auto Value = V; };
 
-template<auto V>
-struct ValueTag{ static constexpr auto Value = V; };
-
-template<class T, T... ts>
-using VTuple = TTuple<ValueTag<ts>...>;
-
-}
+	template<class T, T... ts>
+	using VTuple = type_tuples::TTuple<ValueTag<ts>...>;
+} // namespace value_types
